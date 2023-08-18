0351bbad5a8d23ab59c80aebce3504944582a997
DmitriyLedovskih <dmitryledovskih1@yandex.ru>
2023-08-18 16:53:55 +0700

    fix: исправил баг

--- src/components/LocationModal/LocationModal.jsx
+++ src/components/LocationModal/LocationModal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./LocationModal.css";
-import Button from "./Buttons/Button/Button";
+import Button from "../Buttons/Button/Button";
 
 const LocationModal = () => {
   return (
