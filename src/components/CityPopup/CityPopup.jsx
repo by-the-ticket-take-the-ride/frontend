f9bf4eeedb40a933bf2085295f2faac05e09c712
DmitriyLedovskih <dmitryledovskih1@yandex.ru>
2023-08-21 20:40:13 +0700

    Feat: сделал открытие попапа

--- src/components/CityPopup/CityPopup.jsx
+++ src/components/CityPopup/CityPopup.jsx
@@ -5,17 +5,22 @@ import Button from "../Buttons/Button/Button";
 import CityList from "../CityList/CityList";
 import testData from "./testData.json";
 
-const CityPopup = () => {
+const CityPopup = ({ isActive, onClose }) => {
   const [searchData, setSearchData] = React.useState(testData);
 
   return (
-    <div className="city-popup">
+    <div className={`city-popup ${isActive ? "city-popup_opened" : ""}`}>
       <div className="city-popup__body">
         <div className="city-popup__header">
-          <h2 className="city-popup__title">Выберите город</h2>
-          <Button additionalClass="city-popup__button-close"></Button>
+          <div className="city-popup__row">
+            <h2 className="city-popup__title">Выберите город</h2>
+            <Button
+              additionalClass="city-popup__button-close"
+              onClick={() => onClose(false)}
+            ></Button>
+          </div>
+          <CitySearch data={testData} setSearchData={setSearchData} />
         </div>
-        <CitySearch data={testData} setSearchData={setSearchData} />
         <CityList data={searchData} />
       </div>
     </div>
--- src/components/LocationModal/LocationModal.jsx
+++ src/components/LocationModal/LocationModal.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import "./LocationModal.css";
 import Button from "../Buttons/Button/Button";
 
-const LocationModal = () => {
+const LocationModal = ({ onClick }) => {
   return (
     <div className="location-modal">
       <p className="location-modal__text">Это ваш город?</p>
       <h2 className="location-modal__title">Москва</h2>
       <div className="location-modal__buttons">
-        <Button additionalClass="location-modal__button" light={true}>
+        <Button
+          additionalClass="location-modal__button"
+          light={true}
+          onClick={() => onClick(true)}
+        >
           Другой
         </Button>
         <Button additionalClass="location-modal__button" light={true}>
