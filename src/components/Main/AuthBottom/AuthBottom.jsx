f37d855e54880995642a656abd70ea2e251ea07d
Grigory Griko <griko1996@gmail.com>
2023-08-22 21:06:50 +0300

    feature: закончена верстка для Register

--- src/components/Main/AuthBottom/AuthBottom.jsx
+++ src/components/Main/AuthBottom/AuthBottom.jsx
@@ -3,6 +3,8 @@ import "./AuthBottom.css";
 function AuthBottom() {
   return (
     <div className="popup-auth__footer">
+      {/* элементы пока не используются */}
+      {/*
       <div className="popup-auth__border">
         <div className="popup-auth__stroke-line"></div>
         <p className="popup-auth__border-text text-reset">или</p>
@@ -17,6 +19,7 @@ function AuthBottom() {
         <div className="popup-auth__icon-vk"></div>
         <p className="popup-auth__text-cocial-network text-reset">Войти с ВКонтакте</p>
       </button>
+      */}
 
       <p className="popup-auth__to-login text-reset">
         Уже зарегистрированы? <a href="#" className="popup-auth__to-login-link text-reset">Войти в профиль</a>
