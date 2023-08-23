cef8486bf863b1e274df0be036c2628c4c19b296
VANGAZOR <asei3k@gmail.com>
2023-08-23 09:42:48 +0300

    footer начат

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
