65805d21f8c28be89180ef383c7c402c8a8ed8a5
VANGAZOR <asei3k@gmail.com>
2023-08-23 18:05:42 +0300

    доделан футер

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import "./Footer.scss";
 import footerLogo from "../../assets/images/footer-logo.svg";
+import tiktokLogo from "../../assets/images/tiktok-icon.svg";
+import vkLogo from "../../assets/images/vk-icon.svg";
+
+import okLogo from "../../assets/images/ok-icon.svg";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer__top-box">
-        <div className="footer__top-left-box">
-          <div className="footer__ticket-box">
+      <div className="footer__top">
+        <div className="footer__top-left">
+          <div className="footer__top-left-ticket">
             <img src={footerLogo} alt="Лого проекта" />
-            <div className="footer__ticket-box-right">
-              <p className="footer__title">TICKETERA</p>
-              <p className="footer__subtitle">Афиша событий в вашем городе</p>
+            <div className="footer__top-left-ticket-box">
+              <p className="footer__top-left-ticket-box-title">TICKETERA</p>
+              <p className="footer__top-left-ticket-box-subtitle">
+                Афиша событий в вашем городе
+              </p>
             </div>
           </div>
-          <div className="footer__number-box">
-            <p className="footer__number-box-title">Клиентская поддержка</p>
-            <p className="footer__number-box-number">8 800 000 00 00</p>
+          <div className="footer__top-left-number">
+            <p className="footer__top-left-number-title">
+              Клиентская поддержка
+            </p>
+            <p className="footer__top-left-number-number">8 800 000 00 00</p>
           </div>
         </div>
-        <div className="footer__top-right-box">
+        <div className="footer__top-right">
           <ul>
             <li>Контакты</li>
             <li>Кино</li>
@@ -38,7 +46,14 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="footer__bottom-box"></div>
+      <div className="footer__bottom">
+        <p className="footer__bottom-text">©2023 Все права защищены</p>
+        <div className="footer__bottom-buttons">
+          <img src={tiktokLogo} alt="Тикток лого" />
+          <img src={vkLogo} alt="ВК лого" />
+          <img src={okLogo} alt="ОК лого" />
+        </div>
+      </div>
     </div>
   );
 };
