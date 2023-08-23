4da5601e7a5d3c52533c84bffc58b82bd19bfa0b
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-08-23 16:34:36 +0300

    style: убрала элементы из app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import React from "react";
 import "./App.css";
-import Calendar from "./components/Calendar/Calendar";
-import EventsCardList from "./components/EventsCardList/EventsCardList";
 
 function App() {
-  const [selectedDateEvents, setSelectedDateEvents] = React.useState([]);
-
-  const handleSelectedDateChange = (events) => {
-    setSelectedDateEvents(events);
-  };
-
   return (
     <div className="App">
-      <Calendar handleSelectedDateChange={handleSelectedDateChange} />
-      <EventsCardList selectedDateEvents={selectedDateEvents} />
+
     </div>
   );
 }
