723fa95138b904ee9ffa9604fb8a4db4a22ea243
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-08-26 14:09:47 +0300

    feat: реализовала валидацию

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import './OrderForm.css';
 import Button from "../Buttons/Button/Button";
+import { useFormWithValidation } from "../../utils/useFormWithValidation";
 
 function OrderForm() {
+  const { values, errors, isValid, handleChange } = useFormWithValidation();
+  
   return (
     <div className="order-form">
       <h2 className="order-form__title">Оформление заказа</h2>
@@ -10,34 +13,67 @@ function OrderForm() {
         <div className="personal-details">
           <h3 className="details__title">Личные данные</h3>
           <form className="personal-details__form">
-            <input
-              className="personal-details__input"
-              type="text" 
-              name="name" 
-              id="name"
-              placeholder="Иван"
-            />
-            <input
-              className="personal-details__input"
-              type="text" 
-              name="surname" 
-              id="surname"
-              placeholder="Иванов"
-            />
-            <input
-              className="personal-details__input"
-              type="email" 
-              name="email" 
-              id="email"
-              placeholder="name@mail.ru"
-            />
-            <input
-              className="personal-details__input"
-              type="tel" 
-              name="tel" 
-              id="tel"
-              placeholder="+7 (900) 000-00-00"
-            />
+            <div className="personal-details__input">
+              <input
+                className="personal-details__input-item"
+                type="text" 
+                name="name" 
+                id="name"
+                placeholder="Иван"
+                minLength='2'
+                maxLength='64'
+                pattern="/^[a-zA-Zа-яА-Я\s-]*$"
+                onChange={handleChange}
+                value={values.name || ''}
+                required
+              />
+              <span className="personal-details__input-error">{errors.name}</span>
+            </div>
+            <div className="personal-details__input">
+              <input
+                className="personal-details__input-item"
+                type="text" 
+                name="surname" 
+                id="surname"
+                placeholder="Иванов"
+                minLength='2'
+                maxLength='64'
+                pattern="/^[a-zA-Zа-яА-Я\s-]*$"
+                onChange={handleChange}
+                value={values.surname || ''}
+                required
+              />
+              <span className="personal-details__input-error">{errors.surname}</span>
+            </div>
+            <div className="personal-details__input">
+              <input
+                className="personal-details__input-item"
+                type="email" 
+                name="email" 
+                id="email"
+                placeholder="name@mail.ru"
+                minLength='2'
+                maxLength='64'
+                pattern='[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}'
+                value={values.email || ""}
+                onChange={handleChange}
+                required
+              />
+              <span className="personal-details__input-error">{errors.email}</span>
+            </div>
+            <div className="personal-details__input">
+              <input
+                className="personal-details__input-item"
+                type="tel" 
+                name="tel" 
+                id="tel"
+                placeholder="+7 (900) 000-00-00"
+                value={values.tel || ""}
+                onChange={handleChange}
+                required
+              />
+              <span className="personal-details__input-error">{errors.tel}</span>
+            </div>
           </form>
         </div>
         <div className="order-details">
@@ -55,11 +91,11 @@ function OrderForm() {
           </div>
           <Button
             gradient
-            type="button"
+            type="submit"
           >
             <p className="order-details__buy-btn">Оплатить</p>
           </Button>
-          <p className="order-details__agreement-text">Нажимая «Оплатить», вы принимаете условия <span className="order-details__agreement-text-link">пользовательского соглашения</span></p>
+          <p className="order-details__agreement-text">Нажимая «Оплатить», вы принимаете условия <a className="order-details__agreement-text-link" href="#">пользовательского соглашения</a></p>
         </div>
       </div>
     </div>
