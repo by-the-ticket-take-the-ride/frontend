8f8113035a337efc7b0aa970d3b6f0a300beaaa5
DmitriyLedovskih <dmitryledovskih1@yandex.ru>
2023-08-31 02:45:31 +0700

    merge

--- src/constext/SeatProvider.jsx
+++ src/constext/SeatProvider.jsx
@@ -1,8 +1,8 @@
 import { useLayoutEffect, useState } from "react";
 import { SeatContext } from "./SeatContext";
-import  * as EventApi  from "../utils/currentEventApi";
-import eventJson from '../components/ChoiseThePlace/event.json'
-import ticketsJson from '../components/ChoiseThePlace/tickets.json'
+import * as EventApi from "../utils/currentEventApi";
+import eventJson from "../components/ChoiseThePlace/event.json";
+import ticketsJson from "../components/ChoiseThePlace/tickets.json";
 
 function SeatProvider({ children }) {
   const [totalOrder, setTotalOrder] = useState({});
@@ -10,7 +10,7 @@ function SeatProvider({ children }) {
   const [eventZone, setEventZone] = useState([]);
   const [eventForChoisePlace, setEventForChoisePlace] = useState({});
   const [tickets, setTickets] = useState([]);
-  const [isOpenPopap, setIsOpenPopap] = useState(false)
+  const [isOpenPopap, setIsOpenPopap] = useState(false);
 
   useLayoutEffect(() => {
     // EventApi
@@ -26,51 +26,43 @@ function SeatProvider({ children }) {
     //     }
     //   }).catch(err => console.log(err))
 
-      EventApi
-        .getTickets()
-        .then((tickets) => {
-            if (tickets) {
-              setTickets(tickets)
-            } else {
-              setTickets(ticketsJson)
-            }
-        })
+    EventApi.getTickets().then((tickets) => {
+      if (tickets) {
+        setTickets(tickets);
+      } else {
+        setTickets(ticketsJson);
+      }
+    });
+  }, []);
 
-    },[])
-
-    const handleGetCurrentEvent = (id) => {
-      EventApi
-      .getCurrentEvent(id)
+  const handleGetCurrentEvent = (id) => {
+    EventApi.getCurrentEvent(id)
       .then((event) => {
-        if(event) {
-          setEventZone(event.place.type.zone)
-          setEventForChoisePlace(event)
+        if (event) {
+          setEventZone(event.place.type.zone);
+          setEventForChoisePlace(event);
         } else {
-          setEventZone(eventJson.place.type.zone)
-          setEventForChoisePlace(eventJson)
+          setEventZone(eventJson.place.type.zone);
+          setEventForChoisePlace(eventJson);
         }
-      }).catch(err => console.log(err))
-    }
+      })
+      .catch((err) => console.log(err));
+  };
 
-  return ( 
-    <SeatContext.Provider value={{
-      totalOrder,
-      setTotalOrder,
-      eventZone,
-<<<<<<< HEAD
-      eventForChoisePlace,
-      setEventForChoisePlace,
-      tickets,
-      isOpenPopap,
-      setIsOpenPopap,
-      handleGetCurrentEvent
-=======
-      event,
-      tickets,
-      isOpenPopap,
-      setIsOpenPopap
->>>>>>> feat/popup-payment-success
-    }} >
+  return (
+    <SeatContext.Provider
+      value={{
+        totalOrder,
+        setTotalOrder,
+        eventZone,
+        eventForChoisePlace,
+        setEventForChoisePlace,
+        tickets,
+        isOpenPopap,
+        setIsOpenPopap,
+        handleGetCurrentEvent,
+      }}
+    >
       {children}
     </SeatContext.Provider>
   );
--- src/hooks/usePopupContext.jsx
+++ src/hooks/usePopupContext.jsx
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
 import { useContext } from "react";
 import { PopupContext } from "../constext/PopupContext";
 
-
 function usePopupContext() {
-  const popupContextValue = useContext(PopupContext)
-  return { ...popupContextValue}
+  const popupContextValue = useContext(PopupContext);
+  return { ...popupContextValue };
 }
 
-=======
-import { useContext } from "react";
-import { PopupContext } from "../constext/PopupContext";
-
-
-function usePopupContext() {
-  const popupContextValue = useContext(PopupContext)
-  return { ...popupContextValue}
-}
-
->>>>>>> feat/popup-payment-success
 export default usePopupContext;
